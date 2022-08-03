7c4d7e85dabeac61eebb2ac99c15055ebe72aead
Gohara <gohara.junior@dev.auaha.com.br>
2022-08-03 07:56:25 -0300

    Dropdown finished

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -61,3 +61,30 @@ btnCloseOverlay.addEventListener('click', () => {
 
     tagHtml.classList.remove('show-modal');
 })
+
+// Dropdowns
+const btnMenu = document.querySelectorAll('.js-btn-menu');
+const menuSite = document.querySelectorAll('.js-menu');
+
+btnMenu.forEach((btn, index) => {
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        menuSite.forEach(itemMenu => {
+            itemMenu.classList.remove('active');
+            itemMenu.addEventListener('mouseleave', () => {
+                itemMenu.classList.remove('active');
+                btnMenu.forEach(itemBtn => {
+                    itemBtn.classList.remove('active');
+                })
+            })
+        })
+
+        btnMenu.forEach(itemBtn => {
+            itemBtn.classList.remove('active');
+        })
+
+        btn.classList.add('active');
+        menuSite[index].classList.add('active');
+    })
+})
